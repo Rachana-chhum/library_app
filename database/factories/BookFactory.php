bf3f750096359a41e56c7d1c1acc7ecc195bc678
Nangkhoeum <nangkhoeum.champhai@student.passerellesnumeriques.org>
2025-06-23 16:28:51 +0700

    CRUD Books

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -2,14 +2,19 @@
 
 namespace Database\Factories;
 
-use App\Models\Book;
-use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
 class BookFactory extends Factory
 {
-    protected $model = Book::class;
-
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
     public function definition(): array
     {
         return [
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,16 +4,20 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
 use App\Models\Book;
+use App\Models\Member;
+use App\Models\Librarian;
+use App\Models\Borrower;
+
 
 class DatabaseSeeder extends Seeder
 {
-    public function run(): void
+       public function run(): void
     {
-        // Create 5 categories
         Category::factory()->count(5)->create();
-
-        // Create 20 books linked to those categories
         Book::factory()->count(20)->create();
+        Member::factory()->count(10)->create();
+        Librarian::factory()->count(3)->create();
+        Borrower::factory()->count(15)->create();
     }
 }
 
